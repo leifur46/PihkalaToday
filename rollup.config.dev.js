0c93c8ce30f9ec973779ba3750a8a04c67309533
Klooven <Klooven@users.noreply.github.com>
2018-03-17 11:38:33 +0200

    New JS dev script and production will fail on lint errors. Closes #7

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -2,19 +2,18 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
-import uglify from 'rollup-plugin-uglify';
 
 export default {
   input: "app/today.js",
   output: {
     file: "www/today.js",
-    format: "es"
+    format: "es",
+    sourcemap: "inline"
   },
   plugins: [
     resolve(),
     commonjs(),
     babel({exclude: "node_modules/**"}),
-    eslint({throwOnError: true, throwOnWarning: true}),
-    uglify()
+    eslint()
   ]
 };
